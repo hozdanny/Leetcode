e1e07e8abfe996110239d7d55f0b09ff52e6b45a
hoz.danny <hoz.danny@gmail.com>
2016-02-25 21:45:29 +0800

    add s005

--- src/Solution/S005_LongestPalindrome.java
+++ src/Solution/S005_LongestPalindrome.java
@@ -1,5 +1,18 @@
 package Solution;
 
 public class S005_LongestPalindrome {
-
+	public String longestPalindrome(String s) {
+		int length = s.length();
+		int index = 0;
+		int maxLength = 0;
+		StringBuffer buffer = new StringBuffer();
+		for(int i =0;i<length;i++){
+			buffer.append('#');
+			buffer.append(s.charAt(i));
+		}
+		buffer.append("#$");
+		length = buffer.length();
+		
+		return null;
+	}
 }
